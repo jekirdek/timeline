7540b5b11b0c70647da694d882d5452fbd2ac8fc
Feyzullah SARI <feyzullahsr@gmail.com>
2020-12-06 08:53:52 +0300

    option parameter, screenshot

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # timeline
 Display hours of work as SVG format
+
+
+![alt text](https://github.com/[username]/[reponame]/blob/[branch]/screenshot.png?raw=true)
--- timeline.js
+++ timeline.js
@@ -1,12 +1,17 @@
+function timeline(options){
 
-function timeline(){
+    let defaults = {
+        xAxis : 1400,
+        lineHeight : 40,
+        lineStart : 70,
+        rectHeight : 20,
+        startHour : 8,
+        endHour : 22
+    }
+    let actual = Object.assign({}, defaults, options);
 
-    let xAxis = 1400
-    let lineHeight = 40
-    let lineStart = 70
-    let rectHeight = 20
 
-    var draw = SVG('drawing').size(xAxis, 500)
+    var draw = SVG('drawing').size(actual.xAxis, 500)
 
     const zeroPad = (num, places) => String(num).padStart(places, '0')
 
@@ -27,22 +32,22 @@ function timeline(){
     putHours(8,22)
 
     
-    draw.line(1, lineStart - 10, xAxis -1, lineStart - 10).move(0, lineStart - 10).stroke({ color: '#808080', width: 2, linecap: 'round', opacity: 0.5 })
-    draw.text('Urfa').move(0, lineStart)
-    draw.text('İsmail').move(100,lineStart)
-    draw.text('İbrahim').move(300,lineStart)
-    var survey1 = draw.rect(50, rectHeight).fill('black').radius(5, 5).data('toggle', 'popover').data('content','Çalışma 1').data('container','body').move(568, lineStart)
-    var survey2 = draw.rect(45, rectHeight).fill('black').radius(5, 5).data('toggle', 'popover').data('content','Çalışma 3').data('container','body').move(634, lineStart)
-    lineStart += lineHeight
+    draw.line(1, actual.lineStart - 10, actual.xAxis -1, actual.lineStart - 10).move(0, actual.lineStart - 10).stroke({ color: '#808080', width: 2, linecap: 'round', opacity: 0.5 })
+    draw.text('Urfa').move(0, actual.lineStart)
+    draw.text('İsmail').move(100,actual.lineStart)
+    draw.text('İbrahim').move(300,actual.lineStart)
+    draw.rect(50, actual.rectHeight).fill('black').radius(5, 5).data('toggle', 'popover').data('content','Çalışma 1').data('container','body').move(568, actual.lineStart)
+    draw.rect(45, actual.rectHeight).fill('black').radius(5, 5).data('toggle', 'popover').data('content','Çalışma 3').data('container','body').move(634, actual.lineStart)
+    actual.lineStart += actual.lineHeight
 
-    draw.line(1, lineStart - 10, xAxis -1, lineStart - 10).move(0, lineStart - 10).stroke({ color: '#808080', width: 2, linecap: 'round', opacity: 0.5 })
-    draw.text('İzmir').move(0, lineStart)
-    draw.text('Cihan').move(100,lineStart)
-    draw.text('Emin İlker').move(300,lineStart)
-    draw.rect(90, rectHeight).fill('green').radius(5, 5).data('toggle', 'popover').data('content','Çalışma Lorem').data('container','body').move(545, lineStart)
-    lineStart += lineHeight
+    draw.line(1, actual.lineStart - 10, actual.xAxis -1, actual.lineStart - 10).move(0, actual.lineStart - 10).stroke({ color: '#808080', width: 2, linecap: 'round', opacity: 0.5 })
+    draw.text('İzmir').move(0, actual.lineStart)
+    draw.text('Cihan').move(100,actual.lineStart)
+    draw.text('Emin İlker').move(300, actual.lineStart)
+    draw.rect(90, actual.rectHeight).fill('green').radius(5, 5).data('toggle', 'popover').data('content','Çalışma Lorem').data('container','body').move(545, actual.lineStart)
+    actual.lineStart += actual.lineHeight
 
-    draw.line(1, lineStart - 10, xAxis -1, lineStart - 10).move(0, lineStart - 10).stroke({ color: '#808080', width: 2, linecap: 'round', opacity: 0.5 })
+    draw.line(1, actual.lineStart - 10, actual.xAxis -1, actual.lineStart - 10).move(0, actual.lineStart - 10).stroke({ color: '#808080', width: 2, linecap: 'round', opacity: 0.5 })
 
 
 }
